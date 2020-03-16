1633fe5c904b08a0c67fe7a37d9a4af08364764e
WildSteph <Stephaine.Pantiez@gmail.com>
2020-03-16 09:45:45 +0100

    first commit

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const Namefilm = 'Dragon Ball Super BROLY ';
+const Realisateur = ' Tatsuya Nagamine ';
+const date = ' 14 décembre 2018 '
+const message = Namefilm + Realisateur + date;
+
+// Display a popup
+alert(message);
